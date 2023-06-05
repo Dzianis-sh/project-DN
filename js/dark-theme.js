cbb047d51594639e4c1a441e6c8fccb2ad4c8551
Dzianis Shakh <denshakh92@gmail.com>
2023-06-05 20:56:30 +0300

    add js v2

--- js/dark-theme.js
+++ js/dark-theme.js
@@ -1,5 +1,11 @@
-const btn = document.querySelector('.switch__checkbox');
+// const btn = document.querySelector('.switch__checkbox');
 
-btn.addEventListener('click', function () {
-  document.body.classList.toggle('dark-theme');
+// btn.addEventListener('click', function () {
+//   document.body.classList.toggle('dark-theme');
+// });
+
+window.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.switch__checkbox').addEventListener('click', function () {
+    document.querySelector('body').classList.toggle('dark-theme');
+  });
 });
